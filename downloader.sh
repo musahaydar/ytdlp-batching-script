cc29ced6a96236e4f7430cd56173c1adc31d5940
Musa Haydar <musah@umich.edu>
2023-12-25 13:00:06 -0500

    Print stderr to log and output all failed links to errors file

--- downloader.sh
+++ downloader.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
- 
+set -o pipefail
 filename="list.txt"
  
 for line in $(cat "$filename")
 do
     output=$( cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32 )
-    yt-dlp $line --compat-options filename-sanitization --output "$output.%(ext)s" | tee -a log.txt
+    yt-dlp $line --compat-options filename-sanitization --output "$output.%(ext)s" 2>&1 | tee -a log.txt
+    res=$?
+    if [ $res -ne 0 ]; then
+        echo $line >> errors.txt
+    fi
 done
