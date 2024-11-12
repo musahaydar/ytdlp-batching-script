49672b26d2e18ccf105d9165b0f7e350818872d7
Musa Haydar <musah@umich.edu>
2024-11-12 13:13:12 -0500

    Added path option

--- downloader.sh
+++ downloader.sh
@@ -2,11 +2,15 @@
 set -o pipefail
 filename="list.txt"
 rename=false
+path=""
 
-while getopts ":r" flag; do
+while getopts ":rp:" flag; do
   case $flag in
   r)
     rename=true
+    ;;
+  p)
+    path=${OPTARG}
     ;;
   \?)
     echo "Invalid arguments"
@@ -19,10 +23,10 @@ for line in $(cat "$filename")
 do
   if [ "$rename" = true ] ; then
     output=$( cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32 )
-    yt-dlp $line --compat-options filename-sanitization --output "$output.%(ext)s" 2>&1 | tee -a log.txt
+    yt-dlp $line --compat-options filename-sanitization -P "$path" --output "$output.%(ext)s" 2>&1 | tee -a log.txt
     res=$?
   else 
-    yt-dlp $line --compat-options filename-sanitization 2>&1 | tee -a log.txt
+    yt-dlp $line --compat-options filename-sanitization -P "$path" 2>&1 | tee -a log.txt
     res=$?
   fi
   if [ $res -ne 0 ]; then
